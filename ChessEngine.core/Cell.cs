46a20296a3a8340a840f08990b6ad428af5820f5
Taha Eker <Tahaeker5805@gmail.com>
2025-09-16 09:08:57 +0300

    TouchedCellMovable fonksiyonunu oluşturuldu. Stack OwerFlow sorunu çözülecek

--- ChessEngine.core/Cell.cs
+++ ChessEngine.core/Cell.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Linq;
-
-namespace ChessEngine.Core
+﻿namespace ChessEngine.Core
 {
     public struct Cell
     {
@@ -21,6 +18,8 @@ namespace ChessEngine.Core
         public bool IsWhite => WhitePieces.Contains(Stone);
         public bool IsBlack => BlackPieces.Contains(Stone);
 
+        public bool IsTouchedCellMovable;
+
 
         public Cell(int i, int j, ChessContext ctx)
         {
@@ -34,10 +33,55 @@ namespace ChessEngine.Core
             else
             {
                 Stone = '.';// default empty stone
-
             }
+
+
+            
 
         }
 
+        //public void TouchedCellMovable(int i,int j, ChessContext ctx)
+        //{
+        //    /// Can move Touched from Cell  
+        //    ChessContext tempCtx = BoardState.copyBoard(ctx);
+        //    tempCtx.IsFakeMovement = false;
+        //    bool state = false;
+        //    string from = BoardConverter.IndexToString(i, j);
+        //    for (int rowT = 0; rowT<8; rowT++)
+        //    {
+        //        for (int colT = 0; colT<8; colT++)
+        //        {
+
+        //            string to = BoardConverter.IndexToString(rowT, colT);
+        //            Cell fromCell = BoardConverter.StringToCell(from, tempCtx);
+        //            bool stoneColorW = fromCell.IsWhite ? true : false;
+        //            string error = ErrorChecker.MoveError(from, to, tempCtx);
+        //            if (stoneColorW)
+        //            {
+        //                tempCtx.whiteTurn =true;
+        //                state = error == "" ? true : false;
+        //                if (state)
+        //                    IsTouchedCellMovable =true;
+
+        //            }
+        //            else if (!stoneColorW)
+        //            {
+        //                tempCtx.whiteTurn =false;
+        //                state = error == "" ? true : false;
+        //                if (state)
+        //                    IsTouchedCellMovable =true;
+        //            }
+        //            tempCtx.IsFakeMovement = false;
+
+        //        }
+
+        //    }
+        //}
+
+
+
     }
 }
+
+        
+    
